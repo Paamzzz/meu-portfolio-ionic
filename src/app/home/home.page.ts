7346fad483cef71dff1894097ddd0c883e6d622b
Paamzzz <pamamanciogoulart@gmail.com>
2026-03-24 17:29:50 -0300

    refactor: 'standalone: true' habilitado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,7 @@ import { IonButton, IonHeader, IonToolbar, IonTitle, IonContent, IonImg, IonIcon
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  standalone: true,
   imports: [IonButton,IonHeader, IonToolbar, IonTitle, IonContent, RouterLink, IonImg, IonIcon],
 })
 export class HomePage {
