a2c42a515707a83ce06411ffdfad7a3418105dd2
Paamzzz <pamamanciogoulart@gmail.com>
2026-03-26 19:29:40 -0300

    feat: estruturar html da tela 'sobre' totalmente feita

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,10 +11,6 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
-    path: 'new',
-    loadComponent: () => import('./new/new.page').then( m => m.NewPage)
-  },
-  {
     path: 'sobre',
     loadComponent: () => import('./pages/sobre/sobre.page').then( m => m.SobrePage)
   },
--- src/app/pages/sobre/sobre.page.ts
+++ src/app/pages/sobre/sobre.page.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { star, logoGithub, logoLinkedin } from 'ionicons/icons';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.page.html',
   styleUrls: ['./sobre.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton, IonIcon]
 })
 export class SobrePage implements OnInit {
 
-  constructor() { }
+  constructor() {
+     addIcons({ star, logoGithub, logoLinkedin })
+   }
 
   ngOnInit() {
   }
-
 }
+
+//* Para estudos:
+// import { addIcons } from 'ionicons' → traz a função que popula o dicionário
+// import { starFilled } from 'ionicons/icons' → traz o SVG do ícone
+// IonIcon no imports do @Component → diz pro Angular que você vai usar o componente <ion-icon> no HTML
+// addIcons({ starFilled }) no constructor → registra o ícone no dicionário interno do Ionicons para o IonIcon conseguir encontrá-lo quando o HTML pedir
